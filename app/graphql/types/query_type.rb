4c79fbd5b6e84cd0ae1014528228d977eead5613
Mario Salles <mariofsalles@gmail.com>
2019-04-10 10:29:51 +0100

    Add description, for better documentation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,15 +1,22 @@
 module Types
   class QueryType < BaseObject
 
-    field :test_field, String, null: false
-    def test_field
-      "Hello World!"
+    field :test_field, String, null: false,
+    description: "An example field added by the generator" do
+      argument :name, String, required: true
     end
 
-    field :all_users, [UserType], null: false
-    def all_users
-      User.all
+    def test_field(name:)
+      # Rails.logger.info context[:time] --> insert through controller!! research about it
+      "Hello #{name}!"
     end
-    
+
+    field :user, Types::UserType, null: false, camelize: false,
+    description: 'One user' do
+      argument :id, Integer, required: true
+    end
+    def user(id:)
+      User.where(id: id).first
+    end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,6 +1,5 @@
-module Types
-  class UserType < BaseObject
+class Types::UserType < Types::BaseObject
+  description "An User"
     field :id, Integer, null: false
     field :email, String, null: false
-  end
 end
